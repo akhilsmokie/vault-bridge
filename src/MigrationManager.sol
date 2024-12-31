c445ebcc55d454389576138c3336d5c73966b45b
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2024-12-31 14:58:47 +0100

    feat: migration manager

--- src/MigrationManager.sol
+++ src/MigrationManager.sol
@@ -11,11 +11,10 @@ import {YieldExposedToken} from "./YieldExposedToken.sol";
 import {ILxLyBridge} from "./etc/ILxLyBridge.sol";
 
 /// @title Migration Manager
-/// @dev This token can deposit and bridge to address zero on the corresponding L2. It holds migrated backing and calls yeToken on message receipt.
-// Native Converter should bridge the underlying token to this contract, along with the message, which is claim on LxLy Bridge.
-contract MigrationManager is Initializable, OwnableUpgradeable, PausableUpgradeable {
-    /// @dev Used for cross-chain communication.
-    enum Instruction {
+/// @notice The backing of the custom token on L2s that was minted on L2s is migrated via Native Converter to Migration Manager on the L1, which completes migrations by calling yeToken to mint and bridge yeToken to address zero on those L2s.
+abstract contract MigrationManager is Initializable, OwnableUpgradeable, PausableUpgradeable {
+    /// @dev Used in cross-network communication.
+    enum CrossNetworkInstruction {
         COMPLETE_MIGRATION
     }
 
@@ -37,12 +36,12 @@ contract MigrationManager is Initializable, OwnableUpgradeable, PausableUpgradea
     bytes32 private constant _MIGRATION_MANAGER_STORAGE =
         0xaec447ccc4dc1a1a20af7f847edd1950700343642e68dd8266b4de5e0e190a00;
 
-    event MigrationCompleted(uint32 indexed originNetwork, uint256 amount);
-
     constructor() {
         _disableInitializers();
     }
 
+    /// @param nativeConverter_ The address of Native Converter on L2s.
+    /// @param premigrating_ Plese, see `premigrate`.
     function initialize(
         address owner_,
         address yeToken_,
@@ -50,41 +49,65 @@ contract MigrationManager is Initializable, OwnableUpgradeable, PausableUpgradea
         address nativeConverter_,
         bool premigrating_
     ) external initializer {
+        // Check the inputs.
         require(owner_ != address(0), "INVALID_OWNER");
         require(yeToken_ != address(0), "INVALID_YETOKEN");
         require(lxlyBridge_ != address(0), "INVALID_BRIDGE");
         require(nativeConverter_ != address(0), "INVALID_CONVERTER");
 
+        // Initialize the inherited contracts.
         __Ownable_init(owner_);
         __Pausable_init();
 
         MigrationManagerStorage storage $ = _getMigrationManagerStorage();
 
+        // Initialize the storage.
         $.yeToken = YieldExposedToken(yeToken_);
         $.underlyingToken = IERC20(YieldExposedToken(yeToken_).asset());
         $.lxlyBridge = ILxLyBridge(lxlyBridge_);
         $.nativeConverter = nativeConverter_;
         $.premigrating = premigrating_;
 
+        // Pause the contract if premigrating.
         if (premigrating_) _pause();
 
+        // Approve yeToken.
         $.underlyingToken.approve(yeToken_, type(uint256).max);
     }
 
-    function premigrate(uint32 originNetwork) external onlyOwner {
+    /// @notice Completes migrations of backing from L2s that happened before the STB system existed. If the underlying token had never went through an escrow, premigration may not be needed. Please, consult with the Smart Contracts team.
+    /// @notice Important! Please, read the documentation before using this function!
+    /// @notice First, the legacy contracts must be upgraded and/or paused, such as that the old escrow contract on the L1 does not receive the underlying token anymore (neither by deposits, nor migrations). A premigration plan for each yeToken should be prepared by the Smart Contracts team and approved by Security.
+    /// @notice Second, the entire backing must be transferred from the old escrow contract to this Migration Manager AFTER THE CONTRACT HAS BEEN INITIALIZED (otherwise, anyone could steal the funds by frontrunning the initialization and setting themeselves as the owner).
+    /// @notice Third, the calldata must be prepared such as that the `shares` for each `destinationNetworkId` represent the total amount of yeToken (backing) to mint and lock up in LxLy Bridge for that L2. These amounts must not be inclusive of any transfer fees of the underlying token, and must represent the exact suppy of the custom token minted on an L2 for which the backing was migrated to the L1.
+    /// @notice This function can be called only once - make sure the calldata is correct.
+    /// @notice This function can be called by the owner only.
+    /// @dev `premigrating` must be set to `true` during the initialization in order to use this function.
+    function premigrate(uint32[] calldata destinationNetworkIds, uint256[] calldata shares) external onlyOwner {
         MigrationManagerStorage storage $ = _getMigrationManagerStorage();
 
+        // Check input.
         require($.premigrating, "NOT_PREMIGRATING");
 
-        $.yeToken.depositAndBridge($.underlyingToken.balanceOf(address(this)), address(0), originNetwork, true);
+        // Cache the balance of the underlying token.
+        uint256 balance = $.underlyingToken.balanceOf(address(this));
 
+        // Complete the migrations.
+        // If the underlying token has a transfer fee, you may need to prefund yeToken by transferring some amount of the underlying token to it (not depositing) in order for the migrations to succeed. Please, see `completeMigration` in `YieldExposedToken` for more information.
+        for (uint256 i; i < destinationNetworkIds.length; ++i) {
+            uint256 assets = _convertToAssets(shares[i]);
+            $.yeToken.completeMigration(assets <= balance ? assets : balance, destinationNetworkIds[i], shares[i]);
+            balance = assets <= balance ? balance - assets : 0;
+        }
+
+        // End the premigration and unpause the contract.
         $.premigrating = false;
-
         _unpause();
     }
 
     /// @dev Native Converter on an L2 calls both `bridgeAsset` and `bridgeMessage` on `migrate`.
-    /// @dev The message tells Migration Manager on the L1 how much yeToken must be minted and bridged to adress zero on that L2 in order to equalize the total supply of yeToken and the custom token, and provide enter liquidity on LxLy Bridge on the L1.
+    /// @dev The assets must be claimed before claiming the message.
+    /// @dev The message tells Migration Manager on the L1 how much yeToken must be minted and bridged to adress zero on that L2 in order to equalize the total supply of yeToken and the custom token, and provide liquidity on LxLy Bridge when bridging from L2s.
     function onMessageReceived(address originAddress, uint32 originNetwork, bytes memory data)
         external
         payable
@@ -92,20 +115,25 @@ contract MigrationManager is Initializable, OwnableUpgradeable, PausableUpgradea
     {
         MigrationManagerStorage storage $ = _getMigrationManagerStorage();
 
+        // Check the input.
         require(msg.sender == address($.lxlyBridge), "NOT_LXLY_BRIDGE");
 
-        (Instruction instruction, bytes memory instuctionData) = abi.decode(data, (Instruction, bytes));
+        // Decode the cross-network instruction.
+        (CrossNetworkInstruction instruction, bytes memory instuctionData) =
+            abi.decode(data, (CrossNetworkInstruction, bytes));
 
-        if (instruction == Instruction.COMPLETE_MIGRATION) {
+        // Execute the instruction.
+        if (instruction == CrossNetworkInstruction.COMPLETE_MIGRATION) {
+            // Check the input.
             require(originAddress == $.nativeConverter, "NOT_NATIVE_CONVERTER");
 
-            uint256 amount = abi.decode(instuctionData, (uint256));
+            // Decode the amount.
+            uint256 shares = abi.decode(instuctionData, (uint256));
 
-            $.yeToken.depositAndBridge(amount, address(0), originNetwork, true);
-
-            emit MigrationCompleted(originNetwork, amount);
+            // Complete the migration.
+            $.yeToken.completeMigration(_assetsAfterTransferFee(_convertToAssets(shares)), originNetwork, shares);
         } else {
-            revert("INVALID_INSTRUCTION");
+            revert("INVALID_CROSS_NETWORK_INSTRUCTION");
         }
     }
 
@@ -117,8 +145,18 @@ contract MigrationManager is Initializable, OwnableUpgradeable, PausableUpgradea
     /// @notice Allowes usage of functions with the `whenNotPaused` modifier.
     function unpause() external onlyOwner {
         MigrationManagerStorage storage $ = _getMigrationManagerStorage();
+
+        // Prevent unpausing while premigrating.
         require(!$.premigrating, "PREMIGRATING");
+
+        // Unpause the contract.
         _unpause();
+    }
+
+    /// @notice yeToken is backed by the underlying token 1:1.
+    /// @dev Caution! Changing this function will affect the exchange rate.
+    function _convertToAssets(uint256 shares) internal pure returns (uint256 assets) {
+        assets = shares;
     }
 
     /**
@@ -129,7 +167,14 @@ contract MigrationManager is Initializable, OwnableUpgradeable, PausableUpgradea
             $.slot := _MIGRATION_MANAGER_STORAGE
         }
     }
-}
 
-// @todo Any setters.
-// @todo Polish.
+    /// @notice Accounts for the transfer fee of the underlying token.
+    /// @dev You must implement the same behavior as that of the underlying token for calculating the transfer fee.
+    /// @dev If the underlying token does not have a transfer fee, the output must equal the input.
+    /// @dev This function is used for estamation purposes only.
+    /// @dev Example:
+    /// @dev Fee: 2% flat
+    /// @dev Input: `100`
+    /// @dev Output: `98`
+    function _assetsAfterTransferFee(uint256 assetsBeforeTransferFee) internal view virtual returns (uint256);
+}
