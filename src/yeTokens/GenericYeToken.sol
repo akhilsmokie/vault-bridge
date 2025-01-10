458b9a9f7e24a35bcca6b0bc5fc61da03d64fdc1
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2025-01-10 21:50:36 +0100

    feat: generic yeToken

---
+++ src/yeTokens/GenericYeToken.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity 0.8.28;
+
+import {YieldExposedToken} from "../YieldExposedToken.sol";
+
+/// @title Generic Yield Exposed Token
+/// @dev This contract can be used to deploy yeTokens that do not require any customization, and the underlying token does not have a transfer fee.
+contract GenericYeToken is YieldExposedToken {
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        address owner_,
+        string calldata name_,
+        string calldata symbol_,
+        address underlyingToken_,
+        uint8 minimumReservePercentage_,
+        address yieldVault_,
+        address yieldRecipient_,
+        address lxlyBridge_,
+        address migrationManager_
+    ) external initializer {
+        // Initialize the base implementation.
+        __YieldExposedToken_init(
+            owner_,
+            name_,
+            symbol_,
+            underlyingToken_,
+            minimumReservePercentage_,
+            yieldVault_,
+            yieldRecipient_,
+            lxlyBridge_,
+            migrationManager_
+        );
+    }
+
+    /// @dev The underlying token does not have a transfer fee.
+    function _assetsAfterTransferFee(uint256 assetsBeforeTransferFee)
+        internal
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return assetsBeforeTransferFee;
+    }
+
+    /// @dev The underlying token does not have a transfer fee.
+    function _assetsBeforeTransferFee(uint256 minimumAssetsAfterTransferFee)
+        internal
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        return minimumAssetsAfterTransferFee;
+    }
+}
