4eb5203031924aa1dd5757b87e52504b457bab2a
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2025-01-31 21:23:41 +0100

    chore: remove migration managers

--- src/yield-exposed-tokens/yeUSDC/YeUSDCMigrationManager.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity 0.8.28;
-
-import {GenericMigrationManager} from "../GenericMigrationManager.sol";
-
-/// @title yeUSDC Native Converter
-/// @dev No customization is required.
-/// @dev This contract does not need to be deployed. You can point `YeUSDCMigrationManager` proxy to `GenericMigrationManager` instead.
-contract YeUSDCMigrationManager is GenericMigrationManager {}
