6a4c307ff7b8a481e8889d539612085a9e869502
JDawg287 <sjcool420@hotmail.co.uk>
2025-02-06 00:55:53 -0500

    fix: harmonize initialize function
    Signed-off-by: JDawg287 <sjcool420@hotmail.co.uk>

--- src/yield-exposed-tokens/GenericYeToken.sol
+++ src/yield-exposed-tokens/GenericYeToken.sol
@@ -23,7 +23,7 @@ contract GenericYeToken is YieldExposedToken {
         address yieldVault_,
         address yieldRecipient_,
         address lxlyBridge_,
-        address yeToken_
+        address nativeConverter_
     ) external initializer {
         // Initialize the base implementation.
         __YieldExposedToken_init(
@@ -35,7 +35,7 @@ contract GenericYeToken is YieldExposedToken {
             yieldVault_,
             yieldRecipient_,
             lxlyBridge_,
-            yeToken_
+            nativeConverter_
         );
     }
 
